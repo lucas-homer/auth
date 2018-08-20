98fd91b2a41334fcc068601414d50cac758bf05a
Lucas Homer <lucas.homer@gmail.com>
2018-08-19 20:33:01 -0500

    set up basic routing and header

---
+++ client/src/components/Welcome.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+	return (
+		<div>
+			<h3>Welcome! Sign up or sign in.</h3>
+		</div>
+	);
+}
---
+++ client/src/components/auth/Signup.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+//import { Link } from 'react-router-dom';
+
+class Signup extends Component {
+	render() {
+		return (
+			<form>
+				<fieldset>
+					<label>Email</label>
+
+				</fieldset>
+				<fieldset>
+					<label>Password</label>
+
+				</fieldset>
+			</form>
+		);
+	}
+}
+
+export default Signup;
